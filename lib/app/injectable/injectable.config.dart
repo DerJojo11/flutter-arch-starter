eace4d85d228ea31905231044cf79cc530bbfbc5
Johannes <johannesa@redandroid.io>
2025-11-27 09:07:19 +0100

    remove lint error

--- lib/app/injectable/injectable.config.dart
+++ lib/app/injectable/injectable.config.dart
@@ -9,10 +9,9 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:flutter_arch_starter/app/routes/app_router.dart' as _i874;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
-
-import '../routes/app_router.dart' as _i629;
 
 extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -25,7 +24,7 @@ extension GetItInjectableX on _i174.GetIt {
       environment,
       environmentFilter,
     );
-    gh.lazySingleton<_i629.AppRouter>(() => _i629.AppRouter());
+    gh.lazySingleton<_i874.AppRouter>(() => _i874.AppRouter());
     return this;
   }
 }
